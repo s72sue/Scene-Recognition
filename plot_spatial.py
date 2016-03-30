8290e19cd4034abede1a4eead9412150f578979b
Sugandha <sugandha974@gmail.com>
2016-03-30 17:45:03 -0400

    Added correlation matrix plots

--- plot_spatial.py
+++ plot_spatial.py
@@ -11,12 +11,24 @@ else:
     
 data = pickle.load(open(fname, 'rb')) 
 
+# plotting the correlation matrix
+def plot_correlation(matrix, title):
+    plt.figure(facecolor='white')
+    R = corrcoef(matrix)
+    pcolor(R)
+    colorbar()
+    xticks(arange(0.5, 16.5), range(1,17,1))
+    yticks(arange(0.5, 16.5), range(1,17,1))
+    plt.title(title)
+    show()
+    
 # get corridor db results
 corr_categories = data['corr_categories']
 corr_activations = data['corr_activations']
 corr_prob = data['corr_prob']    
 cf_prob = data['cf_prob']    
-top5_corr = data['top5_corr']    
+top5_corr = data['top5_corr']  
+corr_activations_matrix = data['corr_activations_matrix']
 
 # get forest db results
 forest_categories = data['forest_categories']
@@ -24,6 +36,7 @@ forest_activations = data['forest_activations']
 forest_prob = data['forest_prob']    
 fc_prob = data['fc_prob']    
 top5_forest = data['top5_forest']    
+forest_activations_matrix = data['forest_activations_matrix']
 
 # get conference room db results
 conf_categories = data['conf_categories']
@@ -31,6 +44,7 @@ conf_activations = data['conf_activations']
 conf_prob = data['conf_prob']    
 cocl_prob = data['cocl_prob']    
 top5_conf = data['top5_conf'] 
+conf_activations_matrix = data['conf_activations_matrix']
 
 # get class room db results
 class_categories = data['class_categories']
@@ -38,6 +52,29 @@ class_activations = data['class_activations']
 class_prob = data['class_prob']    
 clco_prob = data['clco_prob']    
 top5_class = data['top5_class'] 
+class_activations_matrix = data['class_activations_matrix']
+
+# plot correlation between Corridor and forest
+for_corr = np.concatenate((forest_activations_matrix, corr_activations_matrix), axis=0)
+
+# plot correlation between Classroom and Conference room
+class_conf = np.concatenate((class_activations_matrix, conf_activations_matrix), axis=0)
+
+"""
+corr = corrcoef(left_right)
+x_values = range(1,9,1)
+y_values = []
+for i in range(len(x_values)):
+    y_values.append(corr[i, i+8]) 
+
+plt.figure(facecolor="white")
+plt.plot(x_values, y_values)
+plt.show()
+"""
+
+plot_correlation(for_corr, "Forest scenes and Corridor image")
+plot_correlation(class_conf, "Classroom scenes and Conference room scenes")
+
 
 print "################################# RESULTS #######################################"
 
@@ -69,4 +106,5 @@ print "Activations: ", class_activations
 print "Probability of Class room: ", class_prob
 print "Probability of Conference room: ", clco_prob
 print "Top 5 Probability: ", top5_class
+
 
